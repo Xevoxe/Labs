2da8c0e8b6d9f194cd3df52667b77f16caf6036c
Joseph Arellano <xevoxe@gmail.com>
2018-07-27 00:09:12 -0500

    Fixed single/double clicking event bug

--- DiceyBusiness/script.js
+++ DiceyBusiness/script.js
@@ -2,12 +2,6 @@ class Dice {
     constructor () {
         this.value = null;
         this.$div = $(`<div><p></p></div>`).addClass('dice')
-        this.$div.click(()=> {
-            this.roll();
-        });
-        this.$div.dblclick(()=>{
-            Dice.remove(this);
-        });
         $("#container-2").prepend(this.$div);
         this.roll();
         this.id = Dice.generateID();
@@ -20,6 +14,8 @@ class Dice {
 
 }
 Dice.list = [];
+//Initialize starting id number
+Dice.ids = 0;
 Dice.generateID = ()=>{
     Dice.ids = Dice.ids + 1;
     return Dice.ids;
@@ -27,6 +23,19 @@ Dice.generateID = ()=>{
 
 Dice.remove = (die)=>{
     die.$div.remove();
+    for(let i = 0; i < Dice.list.length; i++){
+        if(Dice.list[i].id == die.id){
+            Dice.list.splice(i,1);
+        }
+    }
+}
+
+Dice.sum = () => {
+    let sum = 0;
+    for(let i = 0; i < Dice.list.length; i++){
+        sum = Dice.list[i].value + sum;
+    }
+    return sum;
 }
 
 
@@ -38,8 +47,7 @@ $(document).ready(function (){
 
         //Add Die Roll button if there is not dice on the board
         if(Dice.list.length === 0){
-            let btn = $(`<button>Roll Dice!</button>`);
-            btn.click(()=>{
+            let btn = $(`<button>Roll Dice!</button>`).click(()=>{
                 for(let i = 0; i < Dice.list.length; i++){
                     Dice.list[i].roll();
                 }
@@ -47,20 +55,41 @@ $(document).ready(function (){
             $("#container-1").append(btn);
 
             //Add Sum button
-            btn = $(`<button>Sum Dice!</button>`);
-            btn.click(()=> {
-                let sum = 0;
-                for(let i = 0; i < Dice.list.length; i++){
-                    sum = Dice.list[i].value + sum;
-                }
-                alert(sum);
-            });
+            btn = $(`<button>Sum Dice!</button>`).click(()=> alert(Dice.sum()));
+            $("#container-1").append(btn);
 
-            $("#container-1").append(btn)
         }
 
         let die = new Dice();
+
+        //Handle click events for dice elements
+        let clickHandler = createClickHandler(die);
+        die.$div.click(clickHandler);
     
     })
 });
 
+function createClickHandler(die){
+    
+    let timer = null;
+    let delay = 200;
+    let prevent = false;
+
+    function handler(){
+            //If no click event has been triggered track click event and if it has prevent single click event from triggering
+            if(!prevent){       
+            timer = setTimeout( () => {
+                    die.roll();
+                    prevent = false;
+                },delay);
+                prevent = true;
+            }
+            else if(prevent){
+                 clearTimeout(timer);
+                 prevent = false;
+                 Dice.remove(die);
+            }
+
+    }
+    return handler;
+}
